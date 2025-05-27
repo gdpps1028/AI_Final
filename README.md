bdbdd15829f791d5ec368fa5f1f58db375f35555
gdpps1028 <gdpps1028.cs12@nycu.edu.tw>
2025-05-27 22:44:20 +0800

    Added main-main python file for better structure, also updated readme

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # AI_Final
+
+## How to run 
+Change the directory
+```
+cd AI_Final
+```
+Run the file
+```
+python main.py
+```
+Arguments
+```
+'-m', '--Model' // C for Character, R for Radical, S for Stroke
+'-t', '--Train' // Add this flag if you want to retrain the model
+'-e', '--Eval'  // Test the model, input an interger for amount of testcases, default to 100
+```
---
+++ main.py
@@ -0,0 +1,22 @@
+import argparse
+from Character_Only import ch_main
+from Radical_Character import rd_main
+
+parser = argparse.ArgumentParser(description="A sample script using argparse")
+
+parser.add_argument('-m', '--Model', type=str, help='C for Character, R for Radical, S for Stroke', default='C', choices=['C', 'R', 'S'])
+parser.add_argument('-t', '--Train', action='store_true', help='Add this flag if you want to retrain the model')
+parser.add_argument('-e', '--Eval', type=int, help='Test the model, input an interger for amount of testcases', default=100)
+
+args = parser.parse_args()
+
+print(f"Model : {args.Model}")
+print(f"Train : {args.Train}")
+print(f"Eval  : {args.Eval}")
+
+if __name__ == "__main__":
+    if args.Model == 'C':
+        ch_main.main(args.Train, args.Eval)
+    elif args.Model == 'R':
+        rd_main.main(args.Train, args.Eval) 
+    # elif args.Model == 'S':
