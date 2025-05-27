d27f6eb1c0746c62fae6815b72d1d5898aaf0c5f
gdpps1028 <gdpps1028.cs12@nycu.edu.tw>
2025-05-28 01:07:48 +0800

    new model

--- main.py
+++ main.py
@@ -2,10 +2,11 @@ import argparse
 from Character_Only import ch_main
 from Radical_Character import rd_main
 from Stroke_Character import st_main
+from Radical_Stroke import rs_main
 
 parser = argparse.ArgumentParser(description="A sample script using argparse")
 
-parser.add_argument('-m', '--Model', type=str, help='C for Character, R for Radical, S for Stroke', default='C', choices=['C', 'R', 'S'])
+parser.add_argument('-m', '--Model', type=str, help='C for Character, R for Radical, S for Stroke', default='C', choices=['C', 'R', 'S', 'RS'])
 parser.add_argument('-t', '--Train', action='store_true', help='Add this flag if you want to retrain the model')
 parser.add_argument('-e', '--Eval', type=int, help='Test the model, input an interger for amount of testcases', default=100)
 
@@ -22,3 +23,5 @@ if __name__ == "__main__":
         rd_main.main(args.Train, args.Eval) 
     elif args.Model == 'S':
         st_main.main(args.Train, args.Eval) 
+    elif args.Model == 'RS':
+        rs_main.main(args.Train, args.Eval) 
